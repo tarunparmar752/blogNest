dbe5b800a5cb36fb397fe325b32547f16a04b4de
Tarun Parmar <tarunparmar752@gmail.com>
2025-03-11 21:53:18 +0530

    Generated migrations for schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,33 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-app.get('/api/v1/blogsssssssssssssssssssss')
+app.post("/api/v1/user/singup", (c) => {
+  return c.text("User created");
+});
 
-export default app
+app.post("/api/v1/user/signin", (c) => {
+  return c.text("User Logged In");
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("Blog created");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("Blog updated");
+});
+
+app.get("/api/v1/blog/:id", (c) => {
+  return c.text("fetched blog by id");
+});
+
+app.get("/api/v1/blog/bulk", (c) => {
+  return c.text("fetched in bulk");
+});
+
+export default app;
