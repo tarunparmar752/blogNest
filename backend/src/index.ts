a7a80081d715b2a303fea2566c057a80c0541932
Tarun Parmar <tarunparmar752@gmail.com>
2025-03-11 18:45:04 +0530

    initialize backend with basic setup and configuration

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blogsssssssssssssssssssss')
+
+export default app
