5bfb77f87f93652cfd018552766ff5ac9fb7fc63
Tarun Parmar <tarunparmar752@gmail.com>
2025-03-25 07:49:30 +0530

    removed unnecessary lines

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,4 @@
 import { Hono } from "hono";
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
 import { sign, verify } from "hono/jwt";
 import { userRouter } from "./routes/user";
 import { blogRouter } from "./routes/blog";
@@ -15,27 +13,7 @@ const app = new Hono<{
   };
 }>();
 
-app.use("/api/v1/blog/*", async (c, next) => {
-  const jwt = c.req.header("authorization");
-  if (!jwt) {
-		c.status(401);
-		return c.json({ error: "unauthorized" });
-	}
-  const token = jwt.split(" ")[1];
-  const response = await verify(token, c.env.JWT_SECRET);
-  if (response.id) {
-    c.set("userId", response.id.toString());
-    await next();
-  }
-  return c.json({ error: "Unauthorized" }, 401);
-});
-
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/blog", blogRouter);
-
-
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
 
 export default app;
