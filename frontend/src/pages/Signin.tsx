9c973a5ec7cb7989645cbf7f8a7579f29d8ffca9
Tarun Parmar <tarunparmar752@gmail.com>
2025-05-07 09:15:06 +0530

    Add routing and pages for Signin and Signup; update dependencies

---
+++ frontend/src/pages/Signin.tsx
@@ -0,0 +1,9 @@
+function Signin() {
+  return (
+    <div>
+      <h4>Signin</h4>
+    </div>
+  )
+}
+
+export default Signin
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div>
+      <h3>signup</h3>
+    </div>
+  )
+}
+
+export default Signup
